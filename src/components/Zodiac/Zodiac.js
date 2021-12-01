024c4cd61fabe02feb401a67ff0638a38200132a
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-11-30 17:59:44 -0800

    I believe that is destructuring

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -3,9 +3,11 @@ import React from 'react'
 import './Zodiac.css'
 
 export default function Zodiac(props) {
+  const signs = `zodiac ${props.name} ${props.dates}`
+
   return (
     <>
-      <div className="zodiac">
+      <div className={signs}>
         <img src={`${process.env.PUBLIC_URL}/zodiac-signs/${props.name}.png`} />
         <span className="name">{props.name}</span>
         <span>{props.dates}</span>
