91afb169793c354daff7b7f37f0daf4b73b5c061
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-11-30 17:45:11 -0800

    fixing eslint

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map((zodiac) => (
-        <Zodiac key={zodiac.type} {...zodiac} />
+        <Zodiac key={zodiac.name} {...zodiac} />
       ))}
     </main>
   )
